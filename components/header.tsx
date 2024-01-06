f954874b6102eb833a10c0c0e41e169c3a3d6e8e
Raveen Panditha <raveenpanditha9@gmail.com>
2024-01-06 14:56:51 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { motion } from "framer-motion"
 import { links } from "@/lib/data"
+import Link from 'next/link'
 
 export default function Header() {
   return (
@@ -13,9 +14,15 @@ export default function Header() {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         ></motion.div>
 
-        <nav className="fixed top-[0.15rem] left-1/2 h-12 translate-x-1/2 py:2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-            <ul>
-                
+        <nav className="fixed top-[0.15rem] left-1/4 h-12 translate-x-1/3 py:2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+            <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+                {
+                  links.map((link) => (
+                    <li key={link.hash}>
+                      <Link href={link.hash}>{link.name}</Link>
+                    </li>
+                  ))
+                }
             </ul>
         </nav>
     </header>
